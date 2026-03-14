e0d792b55c7dced62ec7fb8c16160a3eed56e5d1
Hüseyin Samet Kel <huseyinsametk@gmail.com>
2026-03-14 22:17:09 +0300

    Faz 1: Manuel geri yükleme sonrası checkpoint (Tüm loglar 40MB parçalı)

--- docs/analiz.py
+++ docs/analiz.py
@@ -1,5 +1,20 @@
 import pandas as pd
 import numpy as np
+import plotly.express as px
 
-steps = pd.read_csv("logs/step_log.csv")
-steps.head()
+
+eps = pd.read_csv(r"C:\Users\husey\Desktop\ads_ai\logs\episode_log.csv")
+
+ep = list()
+sc = list()
+
+for i in range(1, eps.shape[0]):
+    sc_ = eps.loc[1:i]
+    sc__ = sc_[sc_["done_reason"] == "success"].shape[0]
+    scr = sc__ * 100 / sc_.shape[0]
+    ep.append(i)
+    sc.append(scr)
+
+df = pd.DataFrame({"episode": ep, "score": sc})
+fig = px.line(data_frame=df,x="episode",y="score")
+fig.show()
--- scripts/env.py
+++ scripts/env.py
@@ -32,8 +32,8 @@ TARGET_VELOCITY = 0.0
 
 def calculate_new_loc():
     theta = np.random.uniform(0, 2 * np.pi)
-    px = 0 * np.cos(theta)
-    pz = 0 * np.sin(theta)
+    px = 5 * np.cos(theta)
+    pz = 5 * np.sin(theta)
     ry = 180.0
     rz = 90.0 - np.degrees(np.arctan2(pz, px))
     return px, pz, ry, rz
